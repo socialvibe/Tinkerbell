51e712da6317a13cb1f3b417257c90b9447ccf83
Kyle <kyle@truex.com>
2019-11-25 12:26:03 -0800

    adding start button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, Text, View, Button, ToastAndroid } from "react-native";
   
 import TruexAdRendererView from "./components/TruexAdRendererView";
 
@@ -11,14 +11,45 @@ const styles = StyleSheet.create({
 });
 
 export default class App extends React.Component {
-  state = {
+  state = { truexStarted: false };
+
+  constructor(props) {
+    super(props);
+    this._onAdEvent = this._onAdEvent.bind(this);
+  }
+
+  _onAdEvent(key, event) {
+    ToastAndroid.show(key, ToastAndroid.SHORT);
+    if ((key == "AD_COMPLETED") || (key == "NO_ADS_AVAILABLE") || (key == "AD_ERROR") || (key=="OPT_OUT") ) {
+      this.setState({
+        truexStarted: false
+      })
+    }
+    var Platform = require('Platform');
+    var running_on_android_tv = Platform.isTV;
+    ToastAndroid.show(running_on_android_tv? "true":"false", ToastAndroid.SHORT);
   };
 
   render() {
+    // if (this.state.truexStarted) {
+    //   return (
+    //     <View style={styles.container}>
+    //       <TruexAdRendererView style={styles.container} onAdEvent={this._onAdEvent} />
+    //     </View>
+    //   );
+    // }
+
     return (
       <View style={styles.container}>
-        <TruexAdRendererView style={styles.container} />
+        <Button
+          title="Start"
+          onPress={() => {
+            this.setState({
+              truexStarted: true 
+            });
+          }}
+        />
       </View>
-    );
+    )
   }
 }
