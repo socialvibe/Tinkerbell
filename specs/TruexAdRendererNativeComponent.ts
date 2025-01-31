65a09153c2250d2be656d17200588d4ca74ff74b
Ray Blaak <blaak@ensemble.com>
2025-01-30 17:38:43 -0800

    PI-2438: WIP: TAR native component

--- specs/TruexAdRendererNativeComponent.ts
+++ specs/TruexAdRendererNativeComponent.ts
@@ -1,11 +1,46 @@
-import type {HostComponent, ViewProps} from 'react-native';
-import type {BubblingEventHandler} from 'react-native/Libraries/Types/CodegenTypes';
+import {Animated, HostComponent, ViewProps} from 'react-native';
+import type {DirectEventHandler, Double} from 'react-native/Libraries/Types/CodegenTypes';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-import {AdEventHandler, TruexAdEvent} from './TruexAdEvent';
+import Nullable = Animated.Nullable;
+
+export enum TruexAdEventType {
+  AdStarted = 'adStarted',
+  AdDisplayed = 'adDisplayed',
+  AdCompleted = 'adCompleted',
+  AdError = 'adError',
+  NoAdsAvailable = 'noAdsAvailable',
+  AdFreePod = 'adFreePod',
+  AdFetchCompleted = 'adFetchCompleted',
+  UserCancelStream = 'userCancelStream',
+  OptIn = 'optIn',
+  OptOut = 'optOut',
+  SkipCardShown = 'skipCardShown',
+  UserCancel = 'userCancel',
+  XtendedViewStarted = 'xtendedViewStarted',
+  PopupWebsite = "popupWebsite"
+}
+
+export interface TruexAdEvent {
+  eventType: string;
+  timeout?: Double;
+  userInitiated?: boolean;
+  errorMessage?: string;
+}
+
+export function isCompletionEvent(eventType: TruexAdEventType) {
+  switch (eventType) {
+    case TruexAdEventType.NoAdsAvailable:
+    case TruexAdEventType.AdCompleted:
+    case TruexAdEventType.AdError:
+    case TruexAdEventType.UserCancelStream:
+      return true;
+  }
+  return false;
+}
 
 export interface TruexAdRendererProps extends ViewProps {
   vastConfigUrl: string;
-  onAdEvent: AdEventHandler;
+  onAdEvent: DirectEventHandler<TruexAdEvent>;
 }
 
 export default codegenNativeComponent<TruexAdRendererProps>(
